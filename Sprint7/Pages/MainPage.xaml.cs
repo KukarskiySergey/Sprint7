943159620e17cf5b012298e687db6b4d31f912b4
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-12-28 01:54:36 +0500

    Add project files.

---
+++ Sprint7/Pages/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sprint7.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text == "Поиск")
+            {
+                SearchBox.Text = "";
+                SearchBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SearchBox.Text))
+            {
+                SearchBox.Text = "Поиск";
+                SearchBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void TitleBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Проверяем, если нажатие произошло не на кнопках
+            if (!(e.OriginalSource is Button))
+            {
+                NavigationService.Navigate(new Pages.TitlePage());
+            }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Sprint7/Pages/TitlePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sprint7.Pages
+{
+    public partial class TitlePage : Page
+    {
+        public TitlePage()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text == "Поиск")
+            {
+                SearchBox.Text = "";
+                SearchBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SearchBox.Text))
+            {
+                SearchBox.Text = "Поиск";
+                SearchBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void GoBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
